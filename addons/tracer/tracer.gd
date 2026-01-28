322bb1772817e4465c60fb0096ef274d85821c74
Niel McLaren <d@nielmclaren.com>
2026-01-27 17:24:19 -0800

    Add namespace for Tracer Level to prevent naming collisions.

--- addons/tracer/tracer.gd
+++ addons/tracer/tracer.gd
@@ -15,12 +15,12 @@ var span_stack := []
 class Span:
 	extends RefCounted
 
-	var level: Level
+	var level: Tracer.Level
 	var name: String
 	var fields: Dictionary = {}
 	var parent: WeakRef = null
 
-	func _init(parent: Span, level: Level, name: String, fields := {}) -> void:
+	func _init(parent: Span, level: Tracer.Level, name: String, fields := {}) -> void:
 		self.level = level
 		self.name = name
 		self.fields = fields
@@ -40,11 +40,11 @@ class Span:
 		Tracer.span_stack.pop_front()
 		#print_debug(Tracer.span_stack.map(func(s): return s.get_ref().name))
 
-func span(level: Level, name: String, fields := {}) -> Span:
+func span(level: Tracer.Level, name: String, fields := {}) -> Span:
 	return Span.new(null if span_stack.is_empty() else span_stack.front().get_ref(), level, name, fields)
 
 class Trace:
-	var level: Level
+	var level: Tracer.Level
 	var msg: String
 	var module: String = "unknown"
 	var function_name: String = "unknown"
@@ -56,7 +56,7 @@ class Trace:
 	var _has_mono: bool = Engine.has_singleton("GodotSharp")
 
 	func _init(
-		msg: String, level: Level, new_thread_id := 0, fields := {}
+		msg: String, level: Tracer.Level, new_thread_id := 0, fields := {}
 	) -> void:
 		self.msg = msg
 		self.level = level
@@ -87,77 +87,77 @@ func set_current_event(event: Trace) -> void:
 
 func info(msg: String, fields := {}) -> void:
 	current_event = Trace.new(
-		msg, Level.Info, OS.get_thread_caller_id(), fields
+		msg, Tracer.Level.Info, OS.get_thread_caller_id(), fields
 	)
 
 
 func debug(msg: String, fields := {}) -> void:
 	current_event = Trace.new(
-		msg, Level.Debug, OS.get_thread_caller_id(), fields
+		msg, Tracer.Level.Debug, OS.get_thread_caller_id(), fields
 	)
 
 
 func warn(msg: String, fields := {}) -> void:
 	current_event = Trace.new(
-		msg, Level.Warn, OS.get_thread_caller_id(), fields
+		msg, Tracer.Level.Warn, OS.get_thread_caller_id(), fields
 	)
 
 
 func error(msg: String, fields := {}) -> void:
 	current_event = Trace.new(
-		msg, Level.Error, OS.get_thread_caller_id()
+		msg, Tracer.Level.Error, OS.get_thread_caller_id()
 	)
 
 func trace(msg: String, fields := {}) -> void:
-	current_event = Trace.new(msg, Level.Trace, OS.get_thread_caller_id(), fields)
+	current_event = Trace.new(msg, Tracer.Level.Trace, OS.get_thread_caller_id(), fields)
 
 
-static func level_string(level: Level) -> String:
+static func level_string(level: Tracer.Level) -> String:
 	match level:
-		Level.Info:
+		Tracer.Level.Info:
 			return "INFO"
-		Level.Debug:
+		Tracer.Level.Debug:
 			return "DEBUG"
-		Level.Warn:
+		Tracer.Level.Warn:
 			return "WARN"
-		Level.Error:
+		Tracer.Level.Error:
 			return "ERROR"
-		Level.Trace:
+		Tracer.Level.Trace:
 			return "TRACE"
 		_:
 			return "UNKNOWN"
 
 
-static func level_colored(level: Level) -> String:
+static func level_colored(level: Tracer.Level) -> String:
 	var color = ""
 	match level:
-		Level.Info:
+		Tracer.Level.Info:
 			color = "[color=green]%s[/color]"
-		Level.Debug:
+		Tracer.Level.Debug:
 			color = "[color=blue]%s[/color]"
-		Level.Warn:
+		Tracer.Level.Warn:
 			color = "[color=yellow]%s[/color]"
-		Level.Error:
+		Tracer.Level.Error:
 			color = "[color=red]%s[/color]"
-		Level.Trace:
+		Tracer.Level.Trace:
 			color = "[color=magenta]%s[/color]"
 		_:
 			color = "[color=white]%s[/color]"
 	return color % level_string(level)
 
 
-static func level_colored_nice(level: Level) -> String:
+static func level_colored_nice(level: Tracer.Level) -> String:
 	var color = ""
 	match level:
-		Level.Info:
+		Tracer.Level.Info:
 			color = "[color=greenyellow]%s[/color]"
-		Level.Debug:
+		Tracer.Level.Debug:
 			color = "[color=dodgerblue]%s[/color]"
-		Level.Warn:
+		Tracer.Level.Warn:
 			color = "[color=gold]%s[/color]"
-		Level.Error:
+		Tracer.Level.Error:
 			color = "[color=orangered]%s[/color]"
-		Level.Trace:
+		Tracer.Level.Trace:
 			color = "[color=maroon]%s[/color]"
 		_:
 			color = "[color=white]%s[/color]"
